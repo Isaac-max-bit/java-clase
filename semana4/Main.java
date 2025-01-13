3d39d667beec29bf34a35479e2106ea492dc4a41
Your Name <you@example.com>
2025-01-12 22:18:31 -0500

    advance 12/01/2025 10:18

---
+++ semana4/Main.java
@@ -0,0 +1,14 @@
+package semana4;
+
+public class Main {
+    public static void main(String[] args){
+//         String boolStr = "true";
+// boolean boolValue = Boolean.parseBoolean(boolStr);
+// System.out.println(boolValue); // Salida: true
+
+
+String decimalStr = "3.14";
+double decimal = Double.parseDouble(decimalStr);
+System.out.println(decimal); 
+    }
+}
